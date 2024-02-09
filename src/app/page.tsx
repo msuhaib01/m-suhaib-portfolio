32abc0a1dcc4f9747193837fb784ea5dbd819113
msuhaib01 <warheadforehead123@gmail.com>
2024-02-09 17:27:20 +0500

    spacing issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import SectionDivider from "@/components/main/section-divider";
 
 export default function Home() {
   return (
-    <main className="pt-40 sm:pt-28 flex flex-col items-center gap-3">
+    <main className="pt-40 pb-12 sm:pt-28 flex flex-col items-center gap-12">
       <Intro />
       <SectionDivider />
       <About />
--- src/components/main/section-divider.tsx
+++ src/components/main/section-divider.tsx
@@ -5,13 +5,13 @@ export default function SectionDivider() {
   return (
     <div className="flex gap-32">
       <MotionDiv
-        className="bg-stone-200 h-16 w-1 rounded-full hidden sm:block dark:bg-opacity-15"
+        className="bg-stone-200 h-24 w-1 rounded-full hidden sm:block dark:bg-opacity-15"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.225 }}
       ></MotionDiv>
       <MotionDiv
-        className="bg-stone-200 h-16 w-1 rounded-full hidden sm:block dark:bg-opacity-15"
+        className="bg-stone-200 h-24 w-1 rounded-full hidden sm:block dark:bg-opacity-15"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.225 }}
