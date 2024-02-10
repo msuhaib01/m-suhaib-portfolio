1d2859e5fa4002e0e1a3ec8dc5d0fe75290058b0
msuhaib01 <warheadforehead123@gmail.com>
2024-02-10 09:46:35 +0500

    project scaffolds put down

--- src/components/main/about.tsx
+++ src/components/main/about.tsx
@@ -10,17 +10,18 @@ export default function About() {
     >
       <MotionDiv>
         <div className="rounded-sm max-w-[40rem] p-16 bg-[#000000b0] backdrop-blur-md shadow-xl shadow-black flex flex-col gap-3">
-          <h2 className="font-bold text-2xl">About Me</h2>
-          <p>
+          <h2 className="text-center font-bold text-2xl">About Me</h2>
+          <p className="text-justify">
             I spent near a decade living in New York City, picked up quite the
             accent. I've always been interested in computers, from the old days
             when I had a rig with 2gb ram and a Intel Core Duo, to playing
             Minecraft at the lowest settings possible off a USB drive on my
             local library PC.
           </p>
-          <p>
-            Now I'm a Software Engineer for real (And a freelancer!). I've build
-            many fullstack applications using NextJS, Typescript, and React.{" "}
+          <p className="text-justify">
+            Now I'm a Software Engineer for real (And a freelancer!) and I've
+            build many fullstack applications using NextJS, Typescript, and
+            React.
           </p>
         </div>
       </MotionDiv>
--- src/components/main/projects.tsx
+++ src/components/main/projects.tsx
@@ -1,5 +1,31 @@
+"use client";
 import React from "react";
-
+import { projectsData } from "@/lib/data";
+import Image from "next/image";
 export default function Projects() {
-  return <div>projects</div>;
+  return (
+    <section className="max-w-[40rem] flex flex-col">
+      <h2 className="font-bold text-2xl text-center mb-4">Projects</h2>
+      {projectsData.map((project, index) => (
+        <section className="overflow-hidden relative flex flex-col ">
+          <div className="w-1/2 flex flex-col gap-6">
+            <div className="flex flex-col  gap-6">
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+            </div>
+            <div className="flex flex-row flex-wrap gap-x-4 gap-y-2">
+              {project.tags.map((tag, index) => (
+                <div>{tag}</div>
+              ))}
+            </div>
+          </div>
+          <Image
+            alt={project.title}
+            src={project.imageUrl}
+            className="absolute right-[-100px] top-8 w-[400px]"
+          />
+        </section>
+      ))}
+    </section>
+  );
 }
