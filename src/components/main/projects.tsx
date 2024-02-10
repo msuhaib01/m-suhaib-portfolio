52495815e92aa49793db84e7e592468e679bf1df
msuhaib01 <warheadforehead123@gmail.com>
2024-02-10 11:02:52 +0500

    added keys

--- src/components/main/projects.tsx
+++ src/components/main/projects.tsx
@@ -4,19 +4,25 @@ import { projectsData } from "@/lib/data";
 import Image from "next/image";
 export default function Projects() {
   return (
-    <section className="max-w-[48rem] flex flex-col">
-      <h2 className="font-bold text-2xl text-center mb-4">Projects</h2>
+    <section id={"projects"} className="max-w-[48rem] flex flex-col">
+      <h2 className="font-bold text-2xl text-center mb-4 mt-24">Projects</h2>
       <div className="flex flex-col gap-12 p-8">
         {projectsData.map((project, index) => (
-          <section className="group hover:scale-105 hover:bg-[#3b3b3b7c] transition overflow-hidden relative flex flex-col sm:items-start even:sm:items-end items-center rounded-sm bg-black/[0.5] p-8">
+          <section
+            key={project.title}
+            className="group hover:scale-105 hover:bg-[#3b3b3b7c] transition overflow-hidden relative flex flex-col sm:items-start even:sm:items-end items-center rounded-sm bg-black/[0.5] p-8"
+          >
             <div className="sm:w-1/2 flex flex-col gap-6">
               <div className="flex flex-col gap-6">
-                <h3>{project.title}</h3>
+                <h3 className="text-xl font-semibold">{project.title}</h3>
                 <p>{project.description}</p>
               </div>
               <div className="flex flex-row flex-wrap gap-x-4 gap-y-3">
                 {project.tags.map((tag, index) => (
-                  <div className="text-stone-950 font-semibold bg-white/[0.8] py-1 px-3 flex justify-center items-center rounded-full ">
+                  <div
+                    key={tag}
+                    className="text-stone-950 font-semibold bg-white/[0.8] py-1 px-3 text-sm flex justify-center items-center rounded-full "
+                  >
                     {tag}
                   </div>
                 ))}
