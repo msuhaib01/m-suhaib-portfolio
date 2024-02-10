0b129972787c1e8bdb19c2b78970c0ed521f429d
msuhaib01 <warheadforehead123@gmail.com>
2024-02-10 15:40:01 +0500

    animations done and understood using server components and client properly seperated

--- src/components/global/scroll-shrinker.tsx
+++ src/components/global/scroll-shrinker.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { Children } from "react";
+import React from "react";
 import { useRef } from "react";
 import {
   motion,
@@ -18,10 +18,19 @@ export default function ScrollShrinker({
   const shrinkRef = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: shrinkRef,
-    offset: ["0 1", "1.33 1"],
+    offset: ["0 1", "1.25 1"],
+    // "target window" "target window"
+    // 0 means start/top
+    // 1 means end/bottom
+    // first array tells of what 0 should be hence, when targets top meets bottom of window, set 0.
+    // second array tells of what 1 should be hence, when targets bottom meets bottom of window, set 1.
+    // but targets bottom is 1.25 what mean then? we dont want animation to stop moment targets bottom is visible, we want it to
+    // keep going for a bit hence we make it 1.25 so that that point goes a little beyond target bottom to empty spot.
+
+    //think through logically for any other possible wanted combination.
   });
   const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
 
   return (
     <motion.div
