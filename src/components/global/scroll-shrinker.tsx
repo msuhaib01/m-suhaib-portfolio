d27d296b088c188bd636534a2209c5baef2ee2cb
msuhaib01 <warheadforehead123@gmail.com>
2024-02-11 12:16:50 +0500

    Revert "performace fix"
    This reverts commit f5d875aa4410e01c7a7b01cbc4d667a3a915b6b1.

--- src/components/global/scroll-shrinker.tsx
+++ src/components/global/scroll-shrinker.tsx
@@ -16,31 +16,34 @@ export default function ScrollShrinker({
   ...restProps
 }: ScrollShrinkerProps) {
   const shrinkRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: shrinkRef,
+    offset: ["0 1", "1.33 1"],
+    // "target window" "target window"
+    // 0 means start/top
+    // 1 means end/bottom
+    // first array tells of what 0 should be hence, when targets top meets bottom of window, set 0.
+    // second array tells of what 1 should be hence, when targets bottom meets bottom of window, set 1.
+    // but targets bottom is 1.25 what mean then? we dont want animation to stop moment targets bottom is visible, we want it to
+    // keep going for a bit hence we make it 1.25 so that that point goes a little beyond target bottom to empty spot.
+
+    //think through logically for any other possible wanted combination.
+  });
   let isMobile: boolean = true;
   if (typeof window !== "undefined") {
     isMobile = window.innerWidth < 640;
   }
 
-  const { scrollYProgress } = useScroll({
-    target: shrinkRef,
-    offset: ["0 1", "1.33 1"],
-  });
-  // "target window" "target window"
-  // 0 means start/top
-  // 1 means end/bottom
-  // first array tells of what 0 should be hence, when targets top meets bottom of window, set 0.
-  // second array tells of what 1 should be hence, when targets bottom meets bottom of window, set 1.
-  // but targets bottom is 1.25 what mean then? we dont want animation to stop moment targets bottom is visible, we want it to
-  // keep going for a bit hence we make it 1.25 so that that point goes a little beyond target bottom to empty spot.
-
-  //think through logically for any other possible wanted combination.
-
-  const scaleProgess = isMobile
-    ? 1
-    : useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  const opacityProgess = isMobile
-    ? 1
-    : useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+  const scaleProgess = useTransform(
+    scrollYProgress,
+    [0, 1],
+    isMobile ? [1, 1] : [0.8, 1]
+  );
+  const opacityProgess = useTransform(
+    scrollYProgress,
+    [0, 1],
+    isMobile ? [1, 1] : [0.5, 1]
+  );
 
   return (
     <motion.div
